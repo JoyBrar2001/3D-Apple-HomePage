19902601e72e486df3878de4a963cbfe5fe8eb6b
Joy Brar <joybrar2001@gmail.com>
2023-05-22 16:55:48 +0530

    Finished the base

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Jumbotron from './components/Jumbotron'
+import SoundSection from './components/SoundSection'
+import DisplaySection from './components/DisplaySection'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection />
+    </div>
+  )
+}
+
+export default App
